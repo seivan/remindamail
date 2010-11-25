b0e4c5836cccb4605f2cbbfec28b2ab77bdbd2a1
Seivan Heidari <seivan@kth.se>
2010-11-25 20:10:17 +0100

    Facebook update

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'jGZwxdNVRtHn5GD8dR6Eg', 'KBUXeFgVyVIjcDQW70Lnirdd7rNP6orZuPtkSHtV4I'
-  provider :facebook, '155158267853854', '31316b870cd9d016c4f50763d85727d9'
+  provider :facebook, '164395383598978', '94c1883e6557afc51f1ea4f13d28cfff'
 end
